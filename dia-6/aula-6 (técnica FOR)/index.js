17982c5de461b888266ed11b968925a72d141e88
Victor Parente <ovictorparente@outlook.com>
2023-11-21 13:23:51 -0300

    atualização técnica FOR

--- dia-6/aula-6 (técnica FOR)/index.js
+++ dia-6/aula-6 (técnica FOR)/index.js
@@ -1,4 +1,5 @@
 let numeroEscolhido = parseInt(prompt('Escolha um número de 1 a 50'))
+//será mostrado uma contagem de 0 até o número escolhido.
 
 for(number = 0; number <= numeroEscolhido; number++){
     console.log(number)
